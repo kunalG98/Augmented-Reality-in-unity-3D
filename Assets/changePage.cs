66c4151f00b183c98910667df0bbf2ae8a39b28a
Kunal Gupta <kunal.gupta.kunalgupta98@gmail.com>
2018-10-27 19:19:08 +0530

    initial Commit

---
+++ Assets/changePage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changePage : MonoBehaviour {
+
+    public GameObject page1;
+    public GameObject page2;
+
+    private bool page = true;
+
+    public void changepage()
+    {
+        if(page == true)
+        {
+            page = false;
+            page2.SetActive(true);
+            page1.SetActive(false);
+        }
+        else
+        {
+            page = true;
+            page1.SetActive(true);
+            page2.SetActive(false);
+        }
+    }
+}
